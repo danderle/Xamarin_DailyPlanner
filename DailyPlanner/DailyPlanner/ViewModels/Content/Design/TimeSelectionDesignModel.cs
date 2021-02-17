5ac2d3485820c7014f13cc13d4e555bd383f3168
danderle <danderle86@gmail.com>
2021-02-17 07:21:27 +0100

    Added the TimeSelectionContent and corresponding view and design model

---
+++ DailyPlanner/DailyPlanner/ViewModels/Content/Design/TimeSelectionDesignModel.cs
@@ -0,0 +1,26 @@
+﻿namespace DailyPlanner
+{
+    public class TimeSelectionDesignModel : TimeSelectionViewModel
+    {
+        #region Singleton
+
+        /// <summary>
+        /// A single instance of the design model
+        /// </summary>
+        public static TimeSelectionDesignModel Instance = new TimeSelectionDesignModel();
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public TimeSelectionDesignModel()
+        {
+            HeaderLabel = "DesignModel";
+        } 
+
+        #endregion
+    }
+}
---
+++ DailyPlanner/DailyPlanner/ViewModels/Content/TimeSelectionViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DailyPlanner
+{
+    public class TimeSelectionViewModel : BaseViewModel
+    {
+        #region Public Properties
+
+        public int CurrentHour { get; set; } = 0;
+
+        public int CurrentMinute { get; set; } = 0;
+
+        public string HeaderLabel { get; set; } = "";
+
+        public List<int> Hours { get; set; } = new List<int>();
+
+        public List<int> Minutes { get; set; } = new List<int>();
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public TimeSelectionViewModel()
+        {
+            InitializeProperties();
+        }
+
+        /// <summary>
+        /// Overloaded constructor
+        /// </summary>
+        public TimeSelectionViewModel(string headerLabel)
+        {
+            InitializeProperties();
+            HeaderLabel = headerLabel;
+        }
+
+        #endregion
+
+        private void InitializeProperties()
+        {
+            Hours = Enumerable.Range(0, 23).ToList();
+            Minutes = Enumerable.Range(0, 59).ToList();
+        }
+    }
+}
