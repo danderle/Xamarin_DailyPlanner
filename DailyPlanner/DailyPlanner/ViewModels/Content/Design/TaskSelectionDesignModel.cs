e6372b064da8fbca9284ad2760343ded907ca4d4
danderle <danderle86@gmail.com>
2021-02-20 21:20:07 +0100

    added the TaskSelectionContent, TaskSelectionViewModel and TaskSelectionDesignModel

---
+++ DailyPlanner/DailyPlanner/ViewModels/Content/Design/TaskSelectionDesignModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace DailyPlanner
+{
+    public class TaskSelectionDesignModel : TaskSelectionViewModel
+    {
+        #region Singleton
+
+        /// <summary>
+        /// A single instance of the design model
+        /// </summary>
+        public static TaskSelectionDesignModel Instance = new TaskSelectionDesignModel();
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public TaskSelectionDesignModel()
+        {
+            
+        } 
+
+        #endregion
+    }
+}
---
+++ DailyPlanner/DailyPlanner/ViewModels/Content/TaskSelectionViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace DailyPlanner
+{
+    public class TaskSelectionViewModel : BaseViewModel
+    {
+        #region Public Properties
+
+        public bool IsVisible { get; set; } = false;
+
+        public bool DefineCustomTask => string.IsNullOrEmpty(SelectedTask) ? false : SelectedTask.Equals("Other");
+
+        public string SelectedTask { get; set; } = string.Empty;
+
+        public string CustomTask { get; set; } = string.Empty;
+
+        public List<string> PreDefinedTasks { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public TaskSelectionViewModel()
+        {
+            PreDefinedTasks = new List<string>
+            {
+                "Wake up",
+                "Shower",
+                "Brush Teeth",
+                "Eat Breakfast",
+                "Work",
+                "Workout",
+                "Side project",
+                "Other",
+            };
+        } 
+
+        #endregion
+    }
+}
