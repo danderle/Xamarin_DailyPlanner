3ce53f21992394413ce760d0094f1941b69cd221
danderle <danderle86@gmail.com>
2021-03-01 07:56:32 +0100

    Changed the TaskITem to TaskItemViewModel and added TaskItemDesignModel and the TaskItemContent

---
+++ DailyPlanner/DailyPlanner/ViewModels/Content/Design/TaskItemDesignModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace DailyPlanner
+{
+    public class TaskItemDesignModel : TaskItemViewModel
+    {
+        #region Singleton
+
+        /// <summary>
+        /// A single instance of the design model
+        /// </summary>
+        public static TaskItemDesignModel Instance = new TaskItemDesignModel();
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public TaskItemDesignModel()
+        {
+        } 
+
+        #endregion
+    }
+}
---
+++ DailyPlanner/DailyPlanner/ViewModels/Content/TaskItemViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace DailyPlanner
+{
+
+    public class TaskItemViewModel : BaseViewModel
+    {
+        #region Public Properties
+
+        public bool TrashItem { get; set; } = false;
+
+        public bool DeleteVisible { get; set; } = false;
+
+        public string TaskToComplete { get; set; }
+        
+        public TimeSpan StartTime { get; set; }
+
+        public TimeSpan TimeToComplete { get; set; }
+
+        public static event Action ItemTrashed;
+
+        #endregion
+
+        #region Commands
+
+        public ICommand OpenDeleteCommand { get; set; }
+
+        public ICommand CloseDeleteCommand { get; set; }
+
+        public ICommand TrashCommand { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public TaskItemViewModel()
+        {
+            InitializeCommands();
+        }
+
+        #endregion
+
+        #region Commands Methods
+
+        private void OpenDelete()
+        {
+            DeleteVisible = true;
+        }
+
+        private void CloseDelete()
+        {
+            DeleteVisible = false;
+        }
+
+        private void Trash()
+        {
+            TrashItem = true;
+            ItemTrashed?.Invoke();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void InitializeCommands()
+        {
+            OpenDeleteCommand = new RelayCommand(OpenDelete);
+            CloseDeleteCommand = new RelayCommand(CloseDelete);
+            TrashCommand = new RelayCommand(Trash);
+        }
+
+        #endregion
+    }
+}
