4c95af6dee2192a5df6200427ed374f840fcce5e
danderle <danderle86@gmail.com>
2021-03-04 07:20:33 +0100

    Added the BoolToColumnSpanConverter

---
+++ DailyPlanner/DailyPlanner/ValueConverters/BoolToColumnSpanConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace DailyPlanner
+{
+    /// <summary>
+    /// Converts a bool to a ColumnSpan
+    /// </summary>
+    public class BoolToColumnSpan : BaseValueConverter<BoolToColumnSpan>
+    {
+        public override object Convert(object value, Type targetType = null, object parameter = null, CultureInfo culture = null)
+        {
+            bool _value = (bool)value;
+            return _value ? "2" : "1";
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
