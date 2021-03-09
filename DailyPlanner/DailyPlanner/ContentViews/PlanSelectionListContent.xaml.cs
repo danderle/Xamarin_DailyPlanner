8849f8adf5f2786812b5d10996879f16fb4c45e0
danderle <danderle86@gmail.com>
2021-03-09 08:40:43 +0100

    Added a PlanSelectionListContent with a view and design model

---
+++ DailyPlanner/DailyPlanner/ContentViews/PlanSelectionListContent.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DailyPlanner
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PlanSelectionListContent : ContentView
+    {
+        public PlanSelectionListContent()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DailyPlanner/DailyPlanner/ViewModels/Content/PlanSelectionListViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.ObjectModel;
+
+namespace DailyPlanner
+{
+    public class PlanSelectionListViewModel : BaseViewModel
+    {
+        #region Public Properties
+
+        public ObservableCollection<PlanViewModel> PlanSelectionList { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public PlanSelectionListViewModel()
+        {
+        }
+
+        #endregion
+
+        #region Public Methods
+
+
+        #endregion
+    }
+}
