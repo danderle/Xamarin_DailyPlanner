c754aebe825739338e98f3e15417b4abd55790dd
danderle <danderle86@gmail.com>
2021-03-11 08:53:03 +0100

    Styled the SummaryPage and added logic to the View and design model

---
+++ DailyPlanner/DailyPlanner/ViewModels/Pages/Design/SummaryDesignModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace DailyPlanner
+{
+    /// <summary>
+    /// The desig time model to aid in design mode
+    /// </summary>
+    public class SummaryDesignModel : SummaryViewModel
+    {
+        #region Singleton
+
+        /// <summary>
+        /// A single instance of the design model
+        /// </summary>
+        public static SummaryDesignModel Instance = new SummaryDesignModel();
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public SummaryDesignModel() : base(null)
+        {
+            PlanSelectionList = new PlanSelectionListViewModel
+            {
+                Items = new ObservableCollection<PlanViewModel>
+                {
+                    new PlanViewModel
+                    {
+                        TotalTasks = 10,
+                        DaysValid = "Mon, Tue, Wed",
+                        TotalPlannedTime = new TimeSpan(5,20,0),
+                    },
+                    new PlanViewModel
+                    {
+                        TotalTasks = 9,
+                        DaysValid = "Thur, Fri, Sat",
+                        TotalPlannedTime = new TimeSpan(5,20,0),
+                    },
+                    new PlanViewModel
+                    {
+                        TotalTasks = 1,
+                        DaysValid = "Sun",
+                        TotalPlannedTime = new TimeSpan(4,20,0),
+                    },
+                },
+            };
+        } 
+
+        #endregion
+    }
+}
--- DailyPlanner/DailyPlanner/ViewModels/Pages/SummaryViewModel.cs
+++ DailyPlanner/DailyPlanner/ViewModels/Pages/SummaryViewModel.cs
@@ -1,10 +1,56 @@
-﻿namespace DailyPlanner
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+
+namespace DailyPlanner
 {
     public class SummaryViewModel : BaseViewModel
     {
-        public SummaryViewModel()
-        {
+        #region Fields
 
+        private INavigationService navigationService;
+
+        #endregion
+
+        #region Public Properties
+
+        public PlanSelectionListViewModel PlanSelectionList { get; set; } = new PlanSelectionListViewModel();
+
+        #endregion
+
+        #region Commands
+
+        public ICommand AddPlanCommand { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public SummaryViewModel(INavigationService _navigationService)
+        {
+            navigationService = _navigationService;
+            InitializeCommands();
         }
+
+        #endregion
+
+        #region Command Methods
+
+        private async void AddPlanAsync()
+        {
+            // navigate to the Plan page
+            await navigationService.NavigateToAsync<PlanViewModel>();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void InitializeCommands()
+        {
+            AddPlanCommand = new RelayCommand(AddPlanAsync);
+        }
+
+        #endregion
     }
 }
